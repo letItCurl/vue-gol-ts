21a990cba67b6da9f4667f94bf876de13b2cf2d4
rollpz <rolandlopez.developer@gmail.com>
2020-04-06 11:21:57 +0200

    template masonery

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import Sex from '../views/Sex.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/sex',
+    name: 'Sex',
+    component: Sex
   }
 ]
 
---
+++ src/views/Sex.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an sex page</h1>
+  </div>
+</template>
